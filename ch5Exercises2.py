eb0b4ea973ff5e5f76834a335da67e012a5dc261
[Victor Yore] <33044972+eroyrotciv@users.noreply.github.com>
2025-01-10 12:13:02 -0500

    Finished up ch 5 and the exercises using lists, loops, and conditionals.  Verifying lists, comparing, appending, etc.

---
+++ ch5Exercises2.py
@@ -0,0 +1,43 @@
+# conditional loops and if-elif-else statements to work through lists
+usernames = ['vic', 'admin', 'temp', 'steph', 'mike', 'gootch']
+if usernames:
+    for _ in usernames:
+        if _.lower() == 'admin':
+            print(f"Hello {_.title()}, would you like to see a status report?")
+        else:
+            print(f"Hello {_.title()}, what are you working on today?")
+else:
+    print("I'm sorry, it seems I'm having a problem locating your account. " \
+          "Please contact your admin.")
+
+new_users = ['vic', 'admin', 'mike', 'joeseph', 'aaron', 'josh', 'tricky']
+print("\n")
+
+# comparing 2 lists and appending new users to existing users w/o duplicates
+for _ in new_users:
+    if _.lower() in usernames:
+        print(f"Sorry, {_} is already being used, please choose a different " \
+              "username.")
+    elif _.lower() not in usernames:
+        usernames.append(_)
+        print(f"Congratulations, {_} is available. Please choose a password.")
+# showing that values have been appended to users
+print(usernames)
+
+# using ordinal numbers
+nums = []
+for _ in range(1, 10):
+    nums.append(_)
+print(nums) # seeing values appended to list
+if nums:
+    for n in nums:
+        if n == 1:
+            print(f"{n}st")
+        elif n == 2:
+            print(f"{n}nd")
+        elif n == 3:
+            print(f"{n}rd")
+        else:
+            print(f"{n}th")
+else:
+    print("Please add values to the list.")
--- ifWithLists.py
+++ ifWithLists.py
@@ -1,3 +1,4 @@
+"""
 # simple for loop to go through pizza toppings
 requested_toppings = ['mushrooms', 'peppers', 'sausage', 'onions']
 for topping in requested_toppings:
@@ -10,3 +11,28 @@ for topping in requested_toppings:
         print(f"Sorry, {topping} is currently unavailable.")
     else:
         print(f"Adding {topping}.")
+"""
+"""
+# checking to see if a list is empty prior to conditionals
+requested_toppings = []
+
+if requested_toppings:
+    for topping in requested_toppings:
+        print(f"Adding {topping}.")
+    print("Finished making your pizza.")
+else:
+    print("Are you sure you want a plain pizza?")
+"""
+# using multiple lists and checking for list values
+available_toppings = ['mushrooms', 'olives', 'peppers', 'pepperoni',
+                      'sausage', 'onions', 'chicken', 'beef']
+
+requested_toppings = ['onions', 'french fries', 'chicken', 'mushrooms']
+
+for topping in requested_toppings:
+    if topping in available_toppings:
+        print(f"Adding {topping}.")
+    else:
+        print(f"Sorry, we can't put {topping} on this pizza.")
+print("Finished making your pizza.")
+
